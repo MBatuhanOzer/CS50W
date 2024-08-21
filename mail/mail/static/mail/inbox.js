3df93934e9d5beeed77e94bef645f8280e527827
MBatuhanOzer <138161092+MBatuhanOzer@users.noreply.github.com>
2024-08-21 16:02:50 +0300

    Update inbox.js

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,24 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
   document.querySelector('#compose-form').addEventListener('submit', send_email)
 
-  // By default, load the inbox
   load_mailbox('inbox');
 });
 
 function compose_email() {
 
-  // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
   document.querySelector('#mail-view').style.display = 'none';
 
-  // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
@@ -26,13 +22,11 @@ function compose_email() {
 
 function load_mailbox(mailbox) {
 
-  // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#mail-view').style.display = 'none';
 
 
-  // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
 
@@ -42,7 +36,6 @@ function load_mailbox(mailbox) {
     // Print emails
     console.log(emails);
 
-    // ... do something else with emails ...
     emails.forEach( email => {
       const newEmail = document.createElement('div');
       newEmail.className = email.read ? 'list-group-item read': 'list-group-item noread';
@@ -88,10 +81,8 @@ function email_view(id) {
   fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
-    // Print email
     console.log(email);
 
-    // ... do something else with email ...
     document.querySelector('#mail-view').innerHTML = `
     <ul class='list-group'>
       <li class='list-group-item'><strong>From: </strong>${email.sender}</li>
