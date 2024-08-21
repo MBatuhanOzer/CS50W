ba520282d031309bdaf4a6a27ea8c6162e576049
MBatuhanOzer <138161092+MBatuhanOzer@users.noreply.github.com>
2024-08-21 15:58:26 +0300

    Update views.py

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -65,12 +65,10 @@ def create(request):
 def login_view(request):
     if request.method == "POST":
 
-        # Attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
 
-        # Check if authentication successful
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
@@ -92,7 +90,6 @@ def register(request):
         username = request.POST["username"]
         email = request.POST["email"]
 
-        # Ensure password matches confirmation
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
@@ -100,7 +97,6 @@ def register(request):
                 "message": "Passwords must match."
             })
 
-        # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
